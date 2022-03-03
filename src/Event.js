986815061edf0c28b2cc58edbf971c026497ee90
aemullin <amullin68@gmail.com>
2022-03-02 22:05:23 -0800

    Incorporate unit testing for my apps features

---
+++ src/Event.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Event extends Component {
+    state = {
+        hidden: true,
+    };
+
+    render() {
+        return <div className="event">
+
+        </div>;
+    }
+}
+
+export default Event;
