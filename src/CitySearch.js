1dae525c6f46aa96bba2c23a8a49d157619116e9
aemullin <amullin68@gmail.com>
2022-03-09 18:41:45 -0800

    incorporate OOP to create search alerts

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,21 +1,33 @@
 import React, { Component } from 'react';
+import {InfoAlert} from './Alert'
 
 class CitySearch extends Component {
   state = {
     query: '',
     suggestions: [],
-    showSuggestions: undefined
+    showSuggestions: false,
+    infoText: ''
   }
 
   handleInputChanged = (event) => {
       const value = event.target.value;
+      this.setState({showSuggestions:true});
       const suggestions = this.props.locations.filter((location) => {
         return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
       });
-      this.setState({
-        query: value,
-        suggestions,
-      });
+      if (suggestions.length === 0) {
+        this.setState({
+          query: value,
+          infoText: 'We cannot find the city you are looking for.'
+        });
+      } else {
+        return this.setState({
+          query: value,
+          suggestions,
+          infoText: ''
+        });
+      }
+      
   };
 
   handleItemClicked = (suggestion) => {
@@ -39,6 +51,7 @@ class CitySearch extends Component {
           onFocus={() => { this.setState({ showSuggestions: true }) }}
         />
         Filter by City
+        <InfoAlert text={this.state.infoText} />
         <ul className="suggestions" style={this.state.showSuggestions ? {}: { display: 'none' }}>
             {this.state.suggestions.map((suggestion) => (
                 <li
--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
+import {ErrorAlert} from './Alert';
 
 class NumberOfEvents extends Component {
     state = {
         numberOfEvents: '32',
+        infoText: ''
     }
 
     onInputChange = (event) => {
         const value = event.target.value;
-        this.setState({
-            numberOfEvents: value,
-        });
+        if (this.state.numberOfEvents < 1) {
+            this.setState({
+              numberOfEvents: value,
+              infoText: 'Please enter a value from 1 to 32'
+            });
+          } else if (this.state.numberOfEvents > 32) {
+            this.setState({
+              numberOfEvents: value,
+              infoText: 'Please enter a value from 1 to 32'
+            })} else {
+            return this.setState({
+              numberOfEvents: value,
+              infoText: ''
+            });
+        }
         this.props.updateNumberOfEvents(value);
     };
     
@@ -19,6 +33,7 @@ class NumberOfEvents extends Component {
             <div className='event-number'>
                 <input type='number' className='number' value={this.state.numberOfEvents} onChange={this.onInputChange}></input><br/> 
                 Number of events on page
+                <ErrorAlert text={this.state.infoText} />
             </div>
         )
     }
