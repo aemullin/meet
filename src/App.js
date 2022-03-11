c0b29e63e0a2448643ed1ff2f5654c885c3c6560
aemullin <amullin68@gmail.com>
2022-03-10 19:24:29 -0800

    Convert app to PWA, incorporate offline capability and add welcome screen

--- src/App.js
+++ src/App.js
@@ -3,28 +3,48 @@ import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
-import { getEvents, extractLocations } from './api';
+import { getEvents, extractLocations, checkToken, getAccessToken } from './api';
 import './nprogress.css';
+import{WarningAlert} from './alert';
+import WelcomeScreen from './WelcomeScreen';
 
 class App extends Component {
   state = {
     events:[],
     locations: [],
     numberOfEvents: 32,
-    location: 'all'
+    location: 'all',
+    offlineText: '',
+    showWelcomeScreen: undefined
   };
   
-  componentDidMount() {
-    this.mounted = true;
+async componentDidMount() {
+  this.mounted = true;
+  const accessToken = localStorage.getItem('access_token');
+  const isTokenValid = (await checkToken(accessToken)).error ? false : true;
+  const searchParams = new URLSearchParams(window.location.search);
+  const code = searchParams.get("code");
+  this.setState({ showWelcomeScreen: !(code || isTokenValid) });
+  if ((code || isTokenValid) && this.mounted) {
     getEvents().then((events) => {
       if (this.mounted) {
         this.setState({
-          events: events.slice(0, this.state.numberOfEvents),
-          locations: extractLocations(events) 
+           events: events.slice(0, this.state.numberOfEvents), 
+           locations: extractLocations(events) 
         });
       }
     });
   }
+  if (!navigator.onLine){
+    this.setState({
+      offlineText: 'You are not connected to the internet, events may not be up to date'
+    });
+  } else {
+    this.setState({
+      offlineText:''
+    });
+  }
+}
 
   componentWillUnmount(){
     this.mounted = false;
@@ -55,14 +75,19 @@ class App extends Component {
   }
 
   render() {
+    if (this.state.showWelcomeScreen === undefined) return <div className="App" />
+
     return (
       <div className="App">
         <div className="title">
           <a className='title-link' href='https://aemullin.github.io/meet/'><h1>Meet</h1></a>
         </div><br/>
+        <WarningAlert text={this.state.offlineText}/>
         <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} numberOfEvents={this.state.numberOfEvents} />
         <NumberOfEvents updateNumberOfEvents={(number) => {this.updateNumberOfEvents(number)}} />
         <EventList events={this.state.events} numberOfEvents={this.state.numberOfEvents} />
+        <WelcomeScreen showWelcomeScreen={this.state.showWelcomeScreen} getAccessToken={() => { getAccessToken() }} />
+
       </div>
     );
   }
--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import NProgress from 'nprogress';
 
 
-const checkToken = async (accessToken) => {
+export const checkToken = async (accessToken) => {
   const result = await fetch(
     `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
   )
@@ -50,6 +50,11 @@ export const getEvents = async () => {
     return mockData;
   }
 
+  if (!navigator.onLine) {
+    const data = localStorage.getItem("lastEvents");
+    NProgress.done();
+    return data?JSON.parse(data).events:[];;
+  }
 
   const token = await getAccessToken();
 
